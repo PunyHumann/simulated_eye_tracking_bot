4182e95025f9279e034de05bffacc3ae2abff115
PunyHumann <eealvare@andrew.cmu.edu>
2026-05-22 19:20:44 -0400

    fix?

--- ollama_engine.py
+++ ollama_engine.py
@@ -65,28 +65,26 @@ stop_listening = recognizer.listen_in_background(mic, background_callback)
 # OLLAMA CHAT LOOP
 
 # Main loop (press 'q' to quit)
+try:
+    while True:
+        # Ollama Chat with history
+        try:
+            new_message = text_q.get_nowait()
+            print(new_message)
+            chat_response = chat(
+                'CUBE',
+                messages=[*messages, {'role': 'user', 'content': new_message}]
+            )
 
-while True:
-    # Ollama Chat with history
-    try:
-        new_message = text_q.get_nowait()
-        print(new_message)
-        if "exit" in new_message:
-            break
-        chat_response = chat(
-            'CUBE',
-            messages=[*messages, {'role': 'user', 'content': new_message}]
-        )
-
-        # merging past messages with current to conserve history
-        messages += [
-            {'role': 'user', 'content': new_message},
-            {'role': 'assistant', 'content': chat_response.message.content}
-            ]
-        print(chat_response.message.content + '\n')
-        #client_socket.send(chat_response.message.content.encode())
-    except queue.Empty:
+            # merging past messages with current to conserve history
+            messages += [
+                {'role': 'user', 'content': new_message},
+                {'role': 'assistant', 'content': chat_response.message.content}
+                ]
+            print(chat_response.message.content + '\n')
+            #client_socket.send(chat_response.message.content.encode())
+        except queue.Empty:
             continue
-
-stop_listening(wait_for_stop=False)
-#client_socket.close()
+except KeyboardInterrupt:
+    stop_listening(wait_for_stop=False)
+    #client_socket.close()
